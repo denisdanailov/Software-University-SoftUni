afa72935f9d19db8a3e09224b1da53e0153c4980
Okuzmanov <okuzmanov@outlook.com>
2022-04-21 15:53:40 +0300

    Spring Data Feb 2022

--- Spring Data/XML Processing/Exercise/product-shop-xml/src/main/java/com/example/productshopxml/exceptions/ValidationException.java
+++
@@ -1,8 +0,0 @@
-package com.example.productshopxml.exceptions;
-
-public class ValidationException extends RuntimeException{
-
-    public ValidationException(String message) {
-        super(message);
-    }
-}
